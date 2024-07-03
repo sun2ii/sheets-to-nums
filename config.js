a7f2066533268fde2440a3d7ef47251c5e418c88
Ben <benbasuni1@gmail.com>
2024-07-03 14:15:09 -0700

    Init Commit: Creating the first version that can work Features - Need to manually screenshot for PNG - Detects Treble Clef (need to mess with threshold) - Grabs everything on the treble clef
    Moving Forward
    - Grab individual notes and map them to Numbers

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  inputPath: './sheets/png/etude-in-c-101-30.png',
+  outputPath: './output/final-etude-in-c-101-30.png',
+  templateFolder: './trebles',
+  threshold: 0.5,
+};
+
